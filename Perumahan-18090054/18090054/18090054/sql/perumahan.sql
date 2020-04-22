c5a5548a7774ebedda1bad68679b1760857e8f7e
rizkinurfauzi <49502273+rizkinurfauzi@users.noreply.github.com>
2020-04-23 00:45:10 +0700

    Add files via upload

---
+++ Perumahan-18090054/18090054/18090054/sql/perumahan.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2020 at 04:40 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perumahan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_admin`
+--
+
+CREATE TABLE `tb_admin` (
+  `id` int(11) NOT NULL,
+  `nama_lengkap` varchar(50) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_admin`
+--
+
+INSERT INTO `tb_admin` (`id`, `nama_lengkap`, `username`, `password`) VALUES
+(1, 'Administrator', 'admin', '21232f297a57a5a743894a0e4a801fc3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_data`
+--
+
+CREATE TABLE `tb_data` (
+  `id_perumahan` int(11) NOT NULL,
+  `nama_perumahan` varchar(50) NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `no_telp` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `pengembang` varchar(50) NOT NULL,
+  `tipe_perumahan` varchar(50) NOT NULL,
+  `brosur` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_data`
+--
+ALTER TABLE `tb_data`
+  ADD PRIMARY KEY (`id_perumahan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_admin`
+--
+ALTER TABLE `tb_admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tb_data`
+--
+ALTER TABLE `tb_data`
+  MODIFY `id_perumahan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
